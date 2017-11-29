cc0bdcd0eba1e90201e031e69d4dff09f620f256
Alex Milanov <alex@i4web.biz>
2017-11-29 12:45:18 +0200

    refactored a bit

--- index.js
+++ index.js
@@ -2,25 +2,26 @@
 
 const crypto = require('crypto');
 
-const pipe = (a, b) => (...args) => b(a(...args));
-const assignProc = (o, k, proc) =>  Object.assign({}, o, {
-	[k]: proc(o)	
-})
+const pipe = (a, ...fns) => (...args) => fns.reduce((res, fn) => fn(res), a(...args));
+const assign = (o, ...props) => Object.assign({}, o, ...props);
+const keys = o => Object.keys(o);
+
+const log = o => (console.log(o), o);
 
 const hash = o => crypto.createHash('sha256').update(JSON.stringify(o)).digest('hex');
 
-const create = (index, timestamp, data, prevHash) => assignProc(
-	{index, timestamp, data, prevHash}, 'hash', hash
+const create = pipe(
+	(index, timestamp, data, prevHash) => ({index, timestamp, data, prevHash}),
+	o => assign(o, {hash: hash(o)}),
+	log
 );
 
-const genesis = () => create(0, Date.now(), 'Genesis Block', 0)
+const genesis = () => create(0, Date.now(), 'Genesis Block', 0);
 
-const next = (prev) => create(prev.index + 1, Date.now(), `Hi, I am block ${prev.index + 1}`, prev.hash)
+const next = prev => create(prev.index + 1, Date.now(), `Hi, I am block ${prev.index + 1}`, prev.hash);
 
 const chain = [genesis()];
 
 for (let i = 1; i < 10; i++) {
-	chain.push(next(chain[i - 1]))
+	chain.push(next(chain[i - 1]));
 }
-
-console.log(chain);
